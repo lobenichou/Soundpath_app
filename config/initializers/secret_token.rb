88065ef55778093a0f13db1bbdfa9f0615f535aa
laurenbenichou <laurenbenichou@gmail.com>
2014-06-03 14:20:21 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundPath::Application.config.secret_token = '0727e7873c8d067f143e82724624d42b57f887be25aef4c542a83833c949a1bd8f4b01b9cbb0f3b7075b4660bbbbf0be71b65817d87e9a1c36d530ac9149d41e'
